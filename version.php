f42430d1564b81bc4b9e08cf4dea62d87876cd73
james-cnz <5689414+james-cnz@users.noreply.github.com>
2020-01-07 14:44:32 +1300

    Bump version number

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019121300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020010600;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700;        // Requires this Moodle version.
 $plugin->component = 'format_multitopic';    // Full name of the plugin (used for diagnostics). // CHANGED: format name.
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0';
+$plugin->maturity  = MATURITY_RC;
+$plugin->release   = 'v1.1-rc1';
