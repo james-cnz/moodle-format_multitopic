9e4eb22bb2fdb8109e27a3d90d57d81abadd0bd2
james-cnz <5689414+james-cnz@users.noreply.github.com>
2020-06-24 15:02:54 +1200

    v1.1.2-rc3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020062401;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020062402;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700;        // Requires this Moodle version.
 $plugin->component = 'format_multitopic';    // Full name of the plugin (used for diagnostics). // CHANGED: format name.
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = 'v1.1.2-rc2';
+$plugin->release   = 'v1.1.2-rc3';
