fedcfd3486a05ce63d371f3ff7381fcd151e8e57
James C <5689414+james-cnz@users.noreply.github.com>
2022-11-10 15:22:47 +1300

    v4.1.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022111000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022111001;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041900;        // Requires this Moodle version.
 $plugin->component = 'format_multitopic';    // Full name of the plugin (used for diagnostics). // CHANGED: format name.
-$plugin->maturity  = MATURITY_RC;
-$plugin->release   = 'v4.1.0-rc1';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v4.1.0';
