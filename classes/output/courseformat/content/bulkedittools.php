30adc09de4867e344a145bda3d07eaef148ccd4f
James C <5689414+james-cnz@users.noreply.github.com>
2023-05-17 21:30:12 +1200

    PHPUnit test fix

--- classes/output/courseformat/content/bulkedittools.php
+++ classes/output/courseformat/content/bulkedittools.php
@@ -18,71 +18,79 @@ namespace format_multitopic\output\courseformat\content;
 
 use core_courseformat\output\local\content\bulkedittools as bulkedittools_base;
 
-/**
- * Contains the bulk editor tools bar.
- *
- * @package   format_multitopic
- * @copyright 2023 James Calder and Otago Polytechnic
- * @copyright based on work by 2023 Ferran Recio <ferran@moodle.com>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class bulkedittools extends bulkedittools_base {
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+if ($CFG->version >= 2023020300.01) {
 
     /**
-     * Generate the bulk edit control items of a section.
+     * Contains the bulk editor tools bar.
      *
-     * Format plugins can override the method to add or remove elements
-     * from the toolbar.
-     *
-     * @return array of edit control items
+     * @package   format_multitopic
+     * @copyright 2023 James Calder and Otago Polytechnic
+     * @copyright based on work by 2023 Ferran Recio <ferran@moodle.com>
+     * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
-    protected function section_control_items(): array {
-        global $USER;
-        $format = $this->format;
-        $context = $format->get_context();
-        $user = $USER;
+    class bulkedittools extends bulkedittools_base {
 
-        $parentcontrols = parent::section_control_items();
+        /**
+         * Generate the bulk edit control items of a section.
+         *
+         * Format plugins can override the method to add or remove elements
+         * from the toolbar.
+         *
+         * @return array of edit control items
+         */
+        protected function section_control_items(): array {
+            global $USER;
+            $format = $this->format;
+            $context = $format->get_context();
+            $user = $USER;
 
-        $additionalcontrols = [];
+            $parentcontrols = parent::section_control_items();
 
-        if (has_capability('moodle/course:movesections', $context, $user) && !array_key_exists('move', $parentcontrols)) {
-            $additionalcontrols['move'] = [
-                'icon' => 'i/dragdrop',
-                'action' => 'moveSection',
-                'name' => get_string('move', 'moodle'),
-                'title' => $this->format->get_format_string('sectionsmove'),
-                'bulk' => 'section',
-            ];
-        }
+            $additionalcontrols = [];
 
-        $deletecapabilities = ['moodle/course:movesections', 'moodle/course:update'];
-        if (has_all_capabilities($deletecapabilities, $context, $user) && !array_key_exists('delete', $parentcontrols)) {
-            $additionalcontrols['delete'] = [
-                'icon' => 'i/delete',
-                'action' => 'deleteSection',
-                'name' => get_string('delete'),
-                'title' => $this->format->get_format_string('sectionsdelete'),
-                'bulk' => 'section',
-            ];
-        }
+            if (has_capability('moodle/course:movesections', $context, $user) && !array_key_exists('move', $parentcontrols)) {
+                $additionalcontrols['move'] = [
+                    'icon' => 'i/dragdrop',
+                    'action' => 'moveSection',
+                    'name' => get_string('move', 'moodle'),
+                    'title' => $this->format->get_format_string('sectionsmove'),
+                    'bulk' => 'section',
+                ];
+            }
 
-        $addedcontrols = false;
+            $deletecapabilities = ['moodle/course:movesections', 'moodle/course:update'];
+            if (has_all_capabilities($deletecapabilities, $context, $user) && !array_key_exists('delete', $parentcontrols)) {
+                $additionalcontrols['delete'] = [
+                    'icon' => 'i/delete',
+                    'action' => 'deleteSection',
+                    'name' => get_string('delete'),
+                    'title' => $this->format->get_format_string('sectionsdelete'),
+                    'bulk' => 'section',
+                ];
+            }
 
-        $merged = [];
-        foreach ($parentcontrols as $key => $action) {
-            if (!$addedcontrols && $key == 'move' || $key == 'delete') {
+            $addedcontrols = false;
+
+            $merged = [];
+            foreach ($parentcontrols as $key => $action) {
+                if (!$addedcontrols && $key == 'move' || $key == 'delete') {
+                    $merged = array_merge($merged, $additionalcontrols);
+                    $addedcontrols = true;
+                }
+                $merged[$key] = $action;
+            }
+            if (!$addedcontrols) {
                 $merged = array_merge($merged, $additionalcontrols);
                 $addedcontrols = true;
             }
-            $merged[$key] = $action;
-        }
-        if (!$addedcontrols) {
-            $merged = array_merge($merged, $additionalcontrols);
-            $addedcontrols = true;
+
+            return $merged;
         }
 
-        return $merged;
     }
 
 }
