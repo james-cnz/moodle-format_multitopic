2748e7ba03c1e2a2e7da8f661f3a0ff15288c6f9
james-cnz <5689414+james-cnz@users.noreply.github.com>
2023-11-13 09:53:42 +1300

    v4.3.0-rc2

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023102400;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2023102401;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900;         // Requires this Moodle version.
 $plugin->component = 'format_multitopic';    // Full name of the plugin (used for diagnostics). // CHANGED: format name.
 $plugin->maturity = MATURITY_RC;
-$plugin->release = 'v4.3-rc1';
+$plugin->release = 'v4.3.0-rc2';
 $plugin->dependencies = ['format_topics' => 2022041900];
