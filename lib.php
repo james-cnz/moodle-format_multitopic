da559f8025ded82061e49eb6d1767d94d7a1290a
James C <5689414+james-cnz@users.noreply.github.com>
2025-03-29 17:11:13 +1300

    Visibility inheritance fixes WIP 4

--- lib.php
+++ lib.php
@@ -1278,6 +1278,9 @@ class format_multitopic extends core_courseformat\base {
         // Previous to Moodle 4.0 thas logic was hardcoded. To prevent errors in the contrib plugins
         // the default logic is the same required for topics and weeks format and still uses
         // a "hiddensections" format setting.
+        if ($section->component) {
+            return parent::is_section_visible($section);
+        }
         $course = $this->get_course();
         $hidesections = $course->hiddensections ?? true;
         $parent = ($section->section == 0) ?
