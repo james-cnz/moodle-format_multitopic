20ae87113f38430048b1b7b9ac5309fbf37e28cb
James C <5689414+james-cnz@users.noreply.github.com>
2025-06-26 11:19:10 +1200

    Update the section visibility badge (MDL-80220) WIP 3

--- classes/output/courseformat/content/section/visibility.php
+++ classes/output/courseformat/content/section/visibility.php
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 namespace format_multitopic\output\courseformat\content\section;
 
 use section_info;
